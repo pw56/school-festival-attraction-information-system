65ffcc6b66ac4cb5df663cb78e527f20d212f607
Ryu <112185174+pw56@users.noreply.github.com>
2026-07-18 15:31:18 +0900

    開発過程で使用したコードの切り替え用コメントを削除

--- tests/pair-detector/vite.config.ts
+++ tests/pair-detector/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // base: '/tests/pair-detector/dist/',
   base: '/school-festival-attraction-information-system/pair-detector/dist/',
   plugins: [
     tailwindcss(),
