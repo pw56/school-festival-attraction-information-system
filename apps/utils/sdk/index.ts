72f5ce2679adf6c88eb18adb6a9a33886a6da9f7
Ryu <112185174+pw56@users.noreply.github.com>
2026-09-10 20:49:49 +0900

    不整合を修正

--- apps/utils/sdk/index.ts
+++ apps/utils/sdk/index.ts
@@ -1,11 +1,16 @@
 import { HttpClient } from './utils/HttpClient';
+import { EventsApi, AdminEventsApi } from './apis/events';
 import type { ClientOptions } from './types';
 
 export class SdkClient {
   readonly http: HttpClient;
+  readonly events: EventsApi;
+  readonly adminEvents: AdminEventsApi;
 
   constructor(options: ClientOptions) {
     this.http = new HttpClient(options);
+    this.events = new EventsApi(this.http);
+    this.adminEvents = new AdminEventsApi(this.http);
   }
 }
 
@@ -13,3 +18,4 @@ export * from './types';
 export * from './utils/ApiError';
 export * from './utils/HttpClient';
 export * from './utils/validators';
+export * from './apis/events';
