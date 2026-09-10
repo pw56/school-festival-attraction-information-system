3e1e53e27919f02cca374ab1c8f679b5b0c3ec89
Ryu <112185174+pw56@users.noreply.github.com>
2026-09-10 06:15:32 +0000

    仮

--- docs/specs/api/openapi-ts.config.ts
+++ docs/specs/api/openapi-ts.config.ts
@@ -2,13 +2,14 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-  client: '@hey-api/client-fetch',
+  input: '',
+  output: '',
   plugins: [
     '@hey-api/typescript', // 型定義の生成
     {
       name: '@hey-api/sdk',  // SDKの生成プラグイン
       asClass: true,         // クラス形式として出力
-      services: 'tags-split' // tagsごとにファイルを分割してクラス化
+      client: '@hey-api/client-fetch',
     }
   ]
 });
