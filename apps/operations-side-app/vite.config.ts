3fbf33a40aaac59a674532e69da3d3fee9269983
Ryu <112185174+pw56@users.noreply.github.com>
2026-09-11 00:47:55 +0900

    何これ

--- apps/operations-side-app/vite.config.ts
+++ apps/operations-side-app/vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   base: './',
   plugins: [
-    tailwindcss(),
-    svgr()
+    tailwindcss()
   ]
 });
